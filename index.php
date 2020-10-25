200c35892c4cb0f4ab9894a3f961d92f491ea6f0
camilomiranda89 <73410579+camilomiranda89@users.noreply.github.com>
2020-10-24 21:44:04 -0300

    Version 1.0

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once "vistas/parte_superior.php" ?>
+
+<!-- INICIO DEL CONTENIDO PRINCIPAL -->
+<div class="container">
+  <h1>Contenido Principal</h1>
+
+</div>
+
+
+<!-- FIN DEL CONTENIDO PRINCIPAL -->
+
+<?php require_once "vistas/parte_inferior.php" ?>
